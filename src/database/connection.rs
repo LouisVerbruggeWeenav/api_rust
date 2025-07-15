3dfe949774724200f279dd26a210564d35588aec
LouisVerbruggeWeenav <louis.verbrugge@weenav.com>
2025-07-15 10:43:13 +0200

    add dotenv to bdd

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -4,39 +4,39 @@ use mysql::{Opts, Pool, PooledConn};
 
 
 pub struct Connection {
-    pub host: String,
-    pub port: String,
-    pub user: String,
-    pub password: String,
-    pub database: String,
-    pub conn: Option<PooledConn>,
+    host: String,
+    port: String,
+    user: String,
+    password: String,
+    database: String,
+    conn: Option<PooledConn>,
+    url: String
 }
 
 
 impl Connection { 
     pub fn new(host: String, port: String, user: String, password: String, database: String) -> Self {
+
+        let url = format!("mysql://{}:{}@{}:{}/{}", user, password, host, port, database);
         Connection { 
-            host: host,
-            port: port,
-            user: user,
-            password: password,
-            database: database,
+            host,
+            port,
+            user,
+            password,
+            database,
             conn: None,
+            url
             
         }
     }
 
     pub fn connect(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
 
-        // let url: String = format!("mysql://{}:{}@{}:{}/{}", self.user, self.password, self.host, self.port, self.database);
-        let url: &'static str = "mysql://root:welcome1@localhost:3306/boat_directory";
-        let opts: Opts = Opts::from_url(url).expect("Failed to parse URL");
+        let opts: Opts = Opts::from_url(self.url.as_str()).expect("Failed to parse URL");
         let pool: Pool = Pool::new(opts).expect("Failed to create pool");
         let conn: PooledConn = pool.get_conn().expect("Failed to get connection");
         self.conn = Some(conn);
-        
         println!("Connected to the database successfully!");
-
         Ok(())
 
     }
