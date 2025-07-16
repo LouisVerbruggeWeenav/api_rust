dd172d1a46619c76ec4ca4c3f47f88607889ecae
LouisVerbruggeWeenav <louis.verbrugge@weenav.com>
2025-07-16 10:52:15 +0200

    add limit request

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ use serde_json;
 
 use pyo3::prelude::*;
 use pyo3::types::PyModule;
-use std::{f32::consts::E, fs};
+// use std::{f32::consts::E, fs};
 
 use std::ffi::CString;
 
 use std::sync::Mutex;
 use serde_json::Value;
 use serde::{Serialize, Deserialize};
+use actix_web::{middleware::Compress, HttpResponse};
+use tokio::fs;
+
+
 
 
 // Ensure the database module is declared
@@ -59,8 +63,8 @@ struct InfoFrontByName {
 
 #[post("/raspberrypi/data")]
 async fn raspberryData(data: web::Data<AppState>, info: web::Json<InfoRaspberrypi>) ->  impl Responder {
-
-    let data_struct: Value = functionDecryptPython(info.structData.clone()).expect("msg");
+    
+    let data_struct: Value = functionDecryptPython(info.structData.clone()).await.expect("msg");
     let mut boat = data.boat.lock().unwrap();
     boat.add_boat(info.infoBoat.name.clone(), info.infoBoat.startRecord.clone(), info.infoBoat.endRecord.clone(), data_struct);
     "Succes"
@@ -115,46 +119,24 @@ async fn get_boat_by_id_post(data: web::Data<AppState>, info: web::Json<InfoFron
 }
 
 #[post("/boats/one")]
-async fn get_boat_one(data: web::Data<AppState>, info: web::Json<InfoFrontOne>) -> impl Responder {
-    
-    let mut json: serde_json::Value = serde_json::Value::Null;
+async fn get_boat_one() -> impl Responder {
+    let path = "./boats/some_boat.json";
 
-    let mut boat = data.boat.lock().unwrap();
-    match boat.get_boat_by_id(info.id) {
-        Ok(boats) => {           
+    let content = fs::read_to_string(path).await.unwrap_or_default();
+    let json_value: Value = serde_json::from_str(&content).unwrap_or(Value::Null);
 
-            json = match fs::read_to_string(format!("./boats/{}/{}.json", boats.name, boats.path)) {
-                Ok(contenu) => { 
-                    match serde_json::from_str(&contenu) {
-                        Ok(dataJson) => dataJson,
-                        
-                        Err(e) => {
-                            eprintln!("Erreur de parsing JSON : {}", e);
-                            serde_json::json!({ "error": format!("Parsing JSON failed: {}", e) })
-                        }
-                    }
-                },
-                Err(e) => {
-                    let pathFail = format!("./boats/{}/{}.json", boats.name, boats.path);
-                    eprintln!("Erreur de lecture du fichier : {} {}", e, pathFail);
-                    serde_json::json!({ "error": format!("File read failed: {}", e) })
-                }
-            }
-        },
-        Err(e) => {
-            eprintln!("Erreur lors de la récupération : {}", e);
-            json = serde_json::json!({ "error": format!("{}", e) });
-            
-        }
-    }
-
-    Json(json)
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string(&json_value).unwrap())
 }
 
-fn functionDecryptPython(tram_can: String) -> Result<Value, Box<dyn std::error::Error>> {
+async fn functionDecryptPython(tram_can: String) -> Result<Value, Box<dyn std::error::Error>> {
 
     let code_str = fs::read_to_string("./src/decryp/decryp.py")
+        .await
         .expect("Fichier Python introuvable");
+
+
 
     let code_cstring = CString::new(code_str).expect("CString::new failed");
     let filename = CString::new("decryp.py").unwrap();
