6f8ee456de5ab9449211d26136df64fa8bf85739
Verbrugge <louis.verbrugge@weenav.com>
2025-07-28 16:19:49 +0200

    fix error

--- src/main.rs
+++ src/main.rs
@@ -369,29 +369,29 @@ fn functionConcatPython(listPath: Vec<String>) -> Result<serde_json::Value, Box<
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // dotenv().ok();
+    dotenv().ok();
 
-    // let host = env::var("DB_HOST").expect("DB_HOST must be set");
-    // let port = env::var("DB_PORT").expect("DB_PORT must be set");
-    // let user = env::var("DB_USER").expect("DB_USER must be set");
-    // let password = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set");
-    // let database = env::var("DB_DATABASE").expect("DB_DATABASE must be set");
-
-
+    let host = env::var("DB_HOST").expect("DB_HOST must be set");
+    let port = env::var("DB_PORT").expect("DB_PORT must be set");
+    let user = env::var("DB_USER").expect("DB_USER must be set");
+    let password = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set");
+    let database = env::var("DB_DATABASE").expect("DB_DATABASE must be set");
 
 
-    // let database = Connection::new(host, port, user, password, database).expect("Impossible de créer la connexion");
-    // let pool = database.get_pool().clone();
 
-    // let mut boat = Boat::new(Ok(pool));
-    // let config = web::Data::new(AppState { boat: Mutex::new(boat), });
+
+    let database = Connection::new(host, port, user, password, database).expect("Impossible de créer la connexion");
+    let pool = database.get_pool().clone();
+
+    let mut boat = Boat::new(Ok(pool));
+    let config = web::Data::new(AppState { boat: Mutex::new(boat), });
    
     
 
 
     HttpServer::new(move || {
         App::new()
-        // .app_data(config.clone())
+        .app_data(config.clone())
         .app_data(web::PayloadConfig::new(1024 * 1024 * 1024)) // = 1Go
         .service(
             web::scope("/api")  
